2a851f9881c3b3edff035ca8c4d183cf96c4fe0c
Dimonlukhtanov <dimal1999@mail.ru>
2018-04-08 20:50:44 +0700

    Correction of output with commas

--- Brackets.c
+++ Brackets.c
@@ -5,37 +5,38 @@ int main()
 {
 	char choice;
 	char the_chosen[4];
-	int i;
-	
-	char k[]={'\0',',','\0'};
-	char l[2]={',','\0'};
-	
-    for (i=0; i<4; i++)
+	int i,j;
+	char z[]={','};
+	char k[]={'\0'};
+	j=0;
+
+    for (i=0; i<4; i++) 
     	the_chosen[i]=i+1;
+    	
+        
     /*Заполение массива для выдачи цифры 
     какой выбор пользователь сделал*/
 
 	printf("This program checks balance braces, parentheses, brackets, and tags in your code \n\n");
 
-    while (choice != '5'){
+    while (choice != '5') {
         printf ("Какие скобки вы хотите ?\n");
         printf ("1)Breaces: {}\n");
         printf ("2)Parentheses: ( )\n");
         printf ("3)Brackets: {}\n");
         printf ("4)Tags: < >\n");
         printf ("5)Готово \n");
-        printf ("6)Отмена {}\n");
+        printf ("6)Отмена \n");
         
-        i=i+1;
-
-        if (the_chosen!=0) //показывает какой вариант выбран
+        
+       if (the_chosen!=0) //показывает какой вариант выбран
         //printf("Вы ввели: %d\n", the_chosen[1]);
     	printf("Вы ввели: "); //было Ввод
     	scanf ("%s", &choice);
 
-		
-    	
-        switch (choice){
+	j++;
+
+    	switch (choice){
             case '1':
             printf("Один\n");
             //the_chosen=1;
@@ -62,17 +63,23 @@ int main()
             printf("Шесть\n");
             //the_chosen=6;
             break;
-        }
-        
-        	   			          	   
-        	   strncat (k, &choice, 1);
-        	   
-        	   strncat (k, l, 1);
-        	   
-        	   printf("%s", k);
-        	                
-             	
-        	              
+
+            printf("\n");
+            
+        }	
+        	if (j==1)
+        	{
+        		printf("%s",&choice);
+        		strncat(k,&choice,1);
+        	} else
+			           
+		   {
+		       strncat(k,z,1); 
+		   	   printf("%s",k);
+		   	   printf("%s",&choice);
+		   	   strncat(k,&choice,1);
+		   }       
+                      
         printf("\n\n"); //служит для отступа после while(choice)
 
     } 
