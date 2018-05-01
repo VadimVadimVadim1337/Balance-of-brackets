5858e15895c577c9ad33009139cc1d81bf36c3a9
Dimonlukhtanov <dimal1999@mail.ru>
2018-05-01 19:03:20 +0700

    Adding 'of brackets' and moving lines

--- src/print_welcome_message.c
+++ src/print_welcome_message.c
@@ -1,12 +1,24 @@
 #include "print_welcome_message.h"
 void print_welcome_message()
 {
-	blue_color;
-    printf("\t  ████       ███     █          ███     █    ██    ████    ████ \n");             
-    printf("\t  █  █      █   █    █         █   █    █   █ █    █       █    \n");                
-    printf("\t  █████    ███████   █        ███████   █  █  █    █       ████ \n");       
-    printf("\t  █   █     █   █    █         █   █    █ █   █    █       █    \n");         
-    printf("\t  █████     █   █    █████     █   █    ██    █    ████    ████ \n\n");
+    	blue_color;              
+    printf("\t  ████       ███     █          ███     ██    █    ████    ████  \n");           
+    printf("\t  █  █      █   █    █         █   █    █ █   █    █       █     \n");                   
+    printf("\t  █████    ███████   █        ███████   █  █  █    █       ████  \n");         
+    printf("\t  █   █     █   █    █         █   █    █   █ █    █       █     \n");         
+    printf("\t  █████     █   █    █████     █   █    █    ██    ████    ████  \n\n");
+
+    printf("\t   ███      █████ \n");
+    printf("\t  █   █     █     \n");
+    printf("\t █     █    ████  \n");
+    printf("\t  █   █     █     \n");
+    printf("\t   ███      █     \n\n");
+
+    printf("\t  ████     ████      ███      ████    █  █    ████    ███████    ████ \n");
+    printf("\t  █  █     █  █     █   █     █       █ █     █          █       █    \n");
+    printf("\t  █████    ████    ███████    █       ██      ████       █        █   \n");
+    printf("\t  █   █    █ █      █   █     █       █ █     █          █         █  \n");
+    printf("\t  █████    █  █     █   █     ████    █  █    ████       █      ████  \n\n");
     zero_color;
     printf("Эта программа проверяет сблансированность скобок\n\n");                      
 }
