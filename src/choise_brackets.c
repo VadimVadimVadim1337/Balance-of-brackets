355d5f399fe40207df02f5500598896ec15f88e8
Dimonlukhtanov <dimal1999@mail.ru>
2018-05-02 02:02:43 +0700

    Adding a negative version of the validation function

--- src/choise_brackets.c
+++ src/choise_brackets.c
@@ -1,43 +1,27 @@
 #include "choise_brackets.h"
 int choice_brackets()
 {
-    char choice[2];
+    //int choices[4];
+    int choice;
+    int i = 0;
+    int number = checkup_correctness(choice);
     printf ("\nКакие скобки вы хотите ?\n");
     printf ("1)Breaces: {}\n");
     printf ("2)Parentheses: ( )\n");
-    printf ("3)Brackets: {}\n");
+    printf ("3)Brackets: []\n");
     printf ("4)Tags: < >\n");
     printf ("5)Готово \n");
     printf ("6)Отмена \n");
 
 
     printf("\nВыберите: ");
-    scanf("%s",&choice[0]);
+    scanf("%d",&choice);
+    i++;
+    if (number = 228)
+        printf("Вы ввели некорректные данные\n");
 
-    switch(choice[0]){
-        
-        case '1':
-            return 1;
-            break;
-
-        case '2':
-            return 2;
-            break; 
-
-        case '3':
-            return 3;
-            break; 
-
-        case '4':
-            return 4;
-            break; 
-
-        case '5':
-            return 5;
-            break; 
-        
-        printf("\n");
-    }
+    if (choice == 5)
+    return 5;
 
 
 
